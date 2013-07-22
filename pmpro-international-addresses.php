207fe3e630365c8c1ed536bd342a9f65c94daffa
Jason Coleman <jason@strangerstudios.com>
2013-07-22 20:07:22 +0000

    First commit in GitHub repo.

---
+++ pmpro-international-addresses.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: PMPro International Adresses
+Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-international-addresses/
+Description: Add long form addresses to the PMPro checkout.
+Version: .2.2
+Author: Stranger Studios
+Author URI: http://www.strangerstudios.com
+ 
+/*
+	First we need to enable international addresses. We just use the pmpro_international_addresses hook and return true.
+	This will add a "countries" dropdown to the checkout page.
+*/
+function my_pmpro_international_addresses()
+{
+	return true;
+}
+add_filter("pmpro_international_addresses", "my_pmpro_international_addresses");
+
+/*
+	Change some of the billing fields to be not required to support international addresses that don't have a state, etc.
+	Default fields are: bfirstname, blastname, baddress1, bcity, bstate, bzipcode, bphone, bemail, bcountry, CardType, AccountNumber, ExpirationMonth, ExpirationYear, CVV
+*/
+function my_pmpro_required_billing_fields($fields)
+{
+	//remove state and zip
+	unset($fields['bstate']);
+	unset($fields['bzipcode']);
+	
+	return $fields;
+}
+add_filter("pmpro_required_billing_fields", "my_pmpro_required_billing_fields");
+
+/*
+	Make the city, state, and zip/postal code fields show up on their own lines.
+*/
+function my_pmpro_longform_address()
+{
+	return true;
+}
+add_filter("pmpro_longform_address", "my_pmpro_longform_address");
+
+/*
+	(optional) Now we want to change the default country from US to say the United Kingdom (GB)
+	Use the 2-letter acronym.
+*/
+function my_pmpro_default_country($default)
+{	
+	return "GB";
+}
+//add_filter("pmpro_default_country", "my_pmpro_default_country");
+
+/*
+	(optional) You may want to add/remove certain countries from the list. The pmpro_countries filter allows you to do this.
+	The array is formatted like array("US"=>"United States", "GB"=>"United Kingdom"); with the acronym as the key and the full
+	country name as the value.
+*/
+function my_pmpro_countries($countries)
+{
+	//remove the US
+	unset($countries["US"]);
+	
+	//add The Moon (LN short for Lunar?)
+	$countries["LN"] = "The Moon";
+	
+	//You could also rebuild the array from scratch.
+	//$countries = array("CA" => "Canada", "US" => "United States", "GB" => "United Kingdom");
+	
+	return $countries;
+}
+//add_filter("pmpro_countries", "my_pmpro_countries");
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Paid Memberships International Addresses ===
+Contributors: strangerstudios
+Tags: paid memberships pro, addresses, internationa, long form addresses, country
+Requires at least: 3.0
+Tested up to: 3.5.2
+Stable tag: .2.2
+
+Adds long form addresses to the PMPro checkout.
+
+== Description ==
+
+Adds long form addresses and country dropdown to the PMPro checkout.
+
+== Installation ==
+
+1. Make sure you have the Paid Memberships Pro plugin installed and activated.
+1. Upload the `pmpro-international-addresses` directory to the `/wp-content/plugins/` directory of your site.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= I found a bug in the plugin. =
+
+Please post it in the GitHub issue tracker here: https://github.com/strangerstudios/pmpro-international-addresses/issues
+
+= I need help installing, configuring, or customizing the plugin. =
+
+Please visit our premium support site at http://www.paidmembershipspro.com for more documentation and our support forums.
+
+== Changelog ==
+= .2.2 =
+* Upgraded from gist to GitHub plugin.
+* First version with a readme.
